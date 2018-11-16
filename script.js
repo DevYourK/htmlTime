393c61a5d6fae39802afa7aa13831fd8f5551ba2
Troller Your_K <kafu@mail.yourk.me>
2018-11-17 01:27:19 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+var eco_switch = false;
+
+function zeroPlus(num) {
+	return ((num <= 9) ? '0' + num : num);
+}
+
+function time() {
+	var cn = new Date(),
+		cal = cn.getFullYear() + '년 ' + (cn.getMonth() + 1) + "월 " + cn.getDate() + "일",
+		ch_calc = cn.getHours(),
+		am_pm = ((ch_calc >= 12) ? '오후' : '오전'),
+		ch = ((ch_calc > 12) ? zeroPlus(ch_calc - 12): zeroPlus(ch_calc)),
+		cm = zeroPlus(cn.getMinutes()),
+		cs = zeroPlus(cn.getSeconds());
+
+		switch(eco_switch) {
+			case true:
+				var eco = "[ECO 밝기 50%] " ; document.getElementById("block").style.opacity = "0.5";
+			break;
+			case false:
+				if(ch_calc >= 2 && ch_calc <= 7) {
+					if(ch_calc >= 3 && ch_calc <= 6) {
+						var eco = '[밝기 3%] ';
+						document.getElementById("block").style.opacity = "0.03";
+						} else {
+							var eco = '[밝기 15%] ';
+							document.getElementById("block").style.opacity = "0.15";
+					}
+				} else {
+					var eco = ''; document.getElementById("block").style.opacity = "1";
+				}
+			break;
+		}
+
+		document.getElementById("cyear").innerHTML = eco + cal;
+		document.getElementById("ctime").innerHTML = am_pm + '&nbsp;&nbsp;' + ch + ' : ' + cm + ' : ' + cs;
+		setTimeout("time()", 100);
+}
+
+function showMenu() {
+	document.getElementById("menu").style.height = "100%";
+}
+function closeMenu() {
+	document.getElementById("menu").style.height = "0px";
+}
+
+function backgroundPlus() {
+	document.getElementById("block").classList.add("image");
+}
+function backgroundMinus() {
+	document.getElementById("block").classList.remove("image");
+}
+
+function ecoOn() {
+	eco_switch = true;
+}
+function ecoOff() {
+	eco_switch = false;
+}
