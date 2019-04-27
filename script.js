c52b6c3c193c0522ced29dea4e5146d0eb01b82a
Your_K <kafu@yourk.me>
2019-04-27 23:51:51 +0900

    Code Optimize
    I love google fonts

--- script.js
+++ script.js
@@ -1,59 +1,35 @@
-var eco_switch = false;
+var bright=100;
 
 function zeroPlus(num) {
 	return ((num <= 9) ? '0' + num : num);
 }
 
 function time() {
-	var cn = new Date(),
-		cal = cn.getFullYear() + '-' + (cn.getMonth() + 1) + "-" + cn.getDate(),
-		ch_calc = cn.getHours(),
-		am_pm = ((ch_calc >= 12) ? 'PM' : 'AM'),
-		ch = ((ch_calc > 12) ? zeroPlus(ch_calc - 12): zeroPlus(ch_calc)),
-		cm = zeroPlus(cn.getMinutes()),
-		cs = zeroPlus(cn.getSeconds());
+	let cn = new Date();
+	document.getElementById("block").style.opacity = bright/100;
 
-		switch(eco_switch) {
-			case true:
-				var eco = "[50%] " ; document.getElementById("block").style.opacity = "0.5";
-			break;
-			case false:
-				if(ch_calc >= 2 && ch_calc <= 7) {
-					if(ch_calc >= 3 && ch_calc <= 6) {
-						var eco = '[3%] ';
-						document.getElementById("block").style.opacity = "0.03";
-						} else {
-							var eco = '[15%] ';
-							document.getElementById("block").style.opacity = "0.15";
-					}
-				} else {
-					var eco = ''; document.getElementById("block").style.opacity = "1";
-				}
-			break;
-		}
-
-		document.getElementById("cyear").innerHTML = eco + cal;
-		document.getElementById("ctime").innerHTML = am_pm + '&nbsp;&nbsp;' + ch + ' : ' + cm + ' : ' + cs;
-		setTimeout("time()", 100);
+	document.getElementById("cyear").innerHTML = ((bright>=100) ? '' : "["+bright+"%]&nbsp;") + cn.getFullYear() + '-' + zeroPlus(cn.getMonth() + 1) + "-" + zeroPlus(cn.getDate());
+	document.getElementById("ctime").innerHTML = ((cn.getHours() >= 12) ? 'PM' : 'AM') + '&nbsp;' + ((cn.getHours() > 12) ? zeroPlus(cn.getHours() - 12): zeroPlus(cn.getHours())) + ':' + zeroPlus(cn.getMinutes()) + ':' + zeroPlus(cn.getSeconds());
+	setTimeout("time()", 100);
 }
 
-function showMenu() {
+function shoM() {
 	document.getElementById("menu").style.height = "100%";
 }
-function closeMenu() {
+function clsM() {
 	document.getElementById("menu").style.height = "0px";
 }
 
-function backgroundPlus() {
+function iO() {
 	document.getElementById("block").classList.add("image");
 }
-function backgroundMinus() {
+function iD() {
 	document.getElementById("block").classList.remove("image");
 }
 
-function ecoOn() {
-	eco_switch = true;
+function bP() {
+	if(bright<100) bright+=10;
 }
-function ecoOff() {
-	eco_switch = false;
+function bM() {
+	if(bright>=30) bright-=10;
 }
